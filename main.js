ed93c4cbe79668673b57e0621d1cc532ea47ff5e
samandarwebdeveloper <86816538+samandarwebdeveloper@users.noreply.github.com>
2021-09-23 00:31:35 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// HTMLdagi inputlarni chaqirib olamiz
+var elInputForm = document.querySelector(".input-form");
+var elCelciusInput = document.querySelector(".celcius-input");
+var elFarenheitInput = document.querySelector(".farenheit-input");
+
+// HTMLdagi outputni chaqirib olamiz
+var elOutput = document.querySelector(".result-output");
+
+// Funksiya boshlaymiz  
+elCelciusInput.addEventListener('input', function(){
+    var elFahrenheitResult  = parseFloat(elCelciusInput.value.trim(), 10) * 9/5 + 32;
+    elFarenheitInput.value = elFahrenheitResult;
+    elOutput.textContent =  elCelciusInput.value + '°C×9/5+32 =' + elFahrenheitResult;
+});
+
+
+elFarenheitInput.addEventListener("input", function(){
+    var elCelciusResult  = (parseFloat(elFarenheitInput.value.trim(), 10) - 32 ) * 5/9;
+    elCelciusInput.value = elCelciusResult;
+    elOutput.textContent =  elFarenheitInput.value + '°F-32)×5/9 =' + elCelciusResult;
+});
+
+
+
